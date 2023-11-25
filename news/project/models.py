bb23a571c078a1abe0f3971898a52ddcbf7edc26
Papuqq <143436977+Papuqq@users.noreply.github.com>
2023-11-25 22:36:34 +0300

    для пачки
    проблема с миграциями

--- news/project/models.py
+++ news/project/models.py
@@ -4,13 +4,14 @@ from django.core.validators import MinValueValidator
 from django.views.generic import ListView, DetailView
 from django.urls import reverse
 
+
 class Post(models.Model):
     name = models.CharField(
         max_length=50,
         unique=True,
-        added_at=models.DateTimeField(
-            auto_now=True,
-        )
+    )
+    added_at = models.DateTimeField(
+        auto_now=True,
     )
     description = models.TextField()
     quantity = models.IntegerField(
@@ -19,7 +20,7 @@ class Post(models.Model):
     category = models.ForeignKey(
         to='Category',
         on_delete=models.CASCADE,
-        related_name='news', 
+        related_name='news',
     )
     price = models.FloatField(
         validators=[MinValueValidator(0.0)],
@@ -31,9 +32,9 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post_detail', args=[str(self.id)])
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name.title()
-
